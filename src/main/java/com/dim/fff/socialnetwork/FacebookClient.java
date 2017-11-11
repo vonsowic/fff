78cc4780c01ea7075676b79fa0749296ec1ad290
Michał Wąsowicz <mwasowicz7@gmail.com>
2017-11-11 17:25:27 +0100

    Initial commit

---
+++ src/main/java/com/dim/fff/socialnetwork/FacebookClient.java
@@ -0,0 +1,20 @@
+package com.dim.fff.socialnetwork;
+
+import com.restfb.DefaultFacebookClient;
+import com.restfb.Version;
+
+/**
+ * @author miwas
+ * @version 1.0
+ * @since 11.11.17
+ */
+public class FacebookClient {
+
+    private DefaultFacebookClient client = new DefaultFacebookClient(
+            "1132643793537785",
+            "c24119f6f016e515b7443ca633290052",
+            Version.VERSION_2_10);
+
+
+
+}
