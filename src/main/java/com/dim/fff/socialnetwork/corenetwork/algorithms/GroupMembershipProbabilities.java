3c492e3615f5cba817c5807f54e3a4ef87d905d7
belaab <izabela150b@gmail.com>
2018-01-04 20:41:00 +0100

    skeleton for group probabilities updated

--- src/main/java/com/dim/fff/socialnetwork/corenetwork/algorithms/GroupMembershipProbabilities.java
+++ src/main/java/com/dim/fff/socialnetwork/corenetwork/algorithms/GroupMembershipProbabilities.java
@@ -1,16 +1,9 @@
 package com.dim.fff.socialnetwork.corenetwork.algorithms;
-
 import com.dim.fff.socialnetwork.corenetwork.Attributes;
 import com.dim.fff.socialnetwork.corenetwork.Network;
-import com.dim.fff.socialnetwork.dataprovider.dataobjects.Group;
-import com.dim.fff.socialnetwork.dataprovider.stanford.SnapReaderClient;
-import com.google.common.primitives.Longs;
 import org.graphstream.graph.Edge;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Calculates probability for each edge based on group membership
@@ -25,12 +18,11 @@ public class GroupMembershipProbabilities extends BasicAlgorithm {
         super(network);
     }
 
-    Collection<Group> groups = new HashSet<>();
-    SnapReaderClient snapReaderClient = new SnapReaderClient();
 
     private String firstNodeId = new String();
     private String secondNodeId = new String();
 
+    private Map<String, Set<String>> groupsWithUsers = new HashMap<String, Set<String>>();
 
     private Map<String, Object> getMap(String probability, Boolean exist) {
         Map<String, Object> map = new HashMap<String, Object>();
@@ -42,37 +34,42 @@ public class GroupMembershipProbabilities extends BasicAlgorithm {
 
     @Override
     public void compute() {
+        groupsWithUsers= getNetwork().findAllGroupsWithUsers();
 
-        groups = snapReaderClient.getAllGroups();
-
-        groups.forEach((Group singleGroup) -> {
-            long[] group = Longs.toArray(singleGroup.getMembers());
+        for (Set<String> key : groupsWithUsers.values()) {
+            System.out.println(key);
+            String[] group = key.stream().toArray(String[]::new);
 
             for (Integer k = 0; k < group.length - 1; k++) {
-                firstNodeId = String.valueOf(group[k]);
+                firstNodeId = group[k];
+                System.out.println(firstNodeId);
 
                 for (Integer j = k + 1; j < group.length; j++) {
-                    secondNodeId = String.valueOf(group[j]);
+                    secondNodeId = group[j];
+                    System.out.println(secondNodeId);
 
                     if (getNetwork().getGraph().getNode(firstNodeId).hasEdgeBetween(secondNodeId)) {
                         Edge edge = getNetwork().getGraph().getNode(firstNodeId).getEdgeBetween(secondNodeId);
 
-                        Boolean existValue = edge.getAttribute(Attributes.EXISTS);
-                        System.out.println(existValue);
+                        //tutaj pewnie jakies inne liczenie prawdododobienstwa dla tych
+                        //ktorzy sa znajomymi, i dla tych którzy nie są
+                        //Boolean existValue = edge.getAttribute(Attributes.EXISTS);
 
+                        //przykładowe zwiększanie probability, zeby sprawdzic czy dziala
                         String  probability =  edge.getAttribute(Attributes.PROBABILITY).toString();
                         Integer increasedProbability = Integer.parseInt(probability) + 1;
                         edge.addAttribute(Attributes.PROBABILITY, increasedProbability.toString());
 
                     } else {
 
-                        String edgeId = firstNodeId + secondNodeId;
+                        String edgeId = firstNodeId + "---" + secondNodeId;
                         getNetwork().getGraph()
                                 .addEdge(edgeId, firstNodeId, secondNodeId)
                                 .addAttributes(getMap("1", false));
                     }
                 }
             }
-        });
+        }
+
     }
 }
